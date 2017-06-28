766cb9ad4759749863396772b58572ba22f637c5
ven <1265372864@qq.com>
2017-06-28 16:52:36 +0800

    第一次提交

---
+++ webapp-framework-base/src/main/java/com/webapp/framework/base/App.java
@@ -0,0 +1,13 @@
+package com.webapp.framework.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
